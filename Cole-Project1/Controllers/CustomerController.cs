4d61a63447be6b63a596e783cf8c3d1c8f775544
Cole Samuelsen <csamuelsen@wisc.edu>
2021-02-26 19:23:15 -0600

    Add project files.

---
+++ Cole-Project1/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Cole_Project1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cole_Project1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        Customer customer = new Customer();
+    }
+}
---
+++ Cole-Project1/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cole_Project1.Models
+{
+    public class Customer
+    {
+        public int Id { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+
+    }
+}

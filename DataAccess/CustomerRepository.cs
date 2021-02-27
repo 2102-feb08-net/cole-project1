ec4b68bb8da0815e9ad88687b0a2a8eea6dae816
Cole Samuelsen <csamuelsen@wisc.edu>
2021-02-27 14:19:01 -0600

    Starting Organizing

--- DataAccess/CustomerRepository.cs
+++ DataAccess/CustomerRepository.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using Cole_Project1.Models;
+using System;
+using System.Collections.Generic;
 
 namespace DataAccess
 {
     public class CustomerRepository
     {
+
+        public List<Customer> GetAllCustomer()
+        {
+
+        }
     }
 }

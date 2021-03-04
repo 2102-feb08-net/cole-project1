07e7afd3dbb6a5de6dd9cbe10e17c3416643f128
Cole Samuelsen <csamuelsen@wisc.edu>
2021-03-03 20:51:20 -0600

    modal to add new customer

---
+++ Cole-Project1/wwwroot/scipts/addcustomer.js
@@ -0,0 +1,9 @@
+﻿const addcustomerbutton = document.getElementById('addcustomerbutton');
+const addcustomerform = document.getElementById('addcustomerform');
+
+addcustomerbutton.addEventListener('click', event => createPerson(event));
+
+function createPerson(event) {
+    event.preventDefault();
+    console.log("yooo");
+}
